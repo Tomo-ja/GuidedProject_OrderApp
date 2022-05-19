fa14f350715b9558bd287440a3be98aa2b1056d1
Tomo-ja <tomonao.hj@gmail.com>
2022-05-19 14:11:34 -0700

    display menu items

--- GuidedProject_OrderApp/CategoryTableViewController.swift
+++ GuidedProject_OrderApp/CategoryTableViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 @MainActor
 class CategoryTableViewController: UITableViewController {
     
-    let menuController = MenuController()
     var categories = [String]()
 
     override func viewDidLoad() {
@@ -18,7 +17,7 @@ class CategoryTableViewController: UITableViewController {
         
         Task.init{
             do{
-                let categories = try await menuController.fetchCategories()
+                let categories = try await MenuController.shared.fetchCategories()
                 updateUI(with: categories)
             }catch{
                 displayError(error, title: "Failed to Fetch Categories")
--- GuidedProject_OrderApp/MenuItemDetailViewController.swift
+++ GuidedProject_OrderApp/MenuItemDetailViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
+@MainActor
 class MenuItemDetailViewController: UIViewController {
+    
+    let menuItem: MenuItem
+    
+    init?(coder: NSCoder, menuItem: MenuItem){
+        self.menuItem = menuItem
+        super.init(coder: coder)
+    }
+    required init?(coder:NSCoder){
+        fatalError("init(coder:) has not beeen implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 }

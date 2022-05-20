28940449737f38bdc08d76789d05d56547131cee
Tomo-ja <tomonao.hj@gmail.com>
2022-05-19 17:48:57 -0700

    handle badge on tab  bar item

--- GuidedProject_OrderApp/OrderTableViewController.swift
+++ GuidedProject_OrderApp/OrderTableViewController.swift
@@ -12,6 +12,8 @@ class OrderTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         NotificationCenter.default.addObserver(tableView!, selector: #selector(UITableView.reloadData), name: MenuController.orderUpdateNotification, object: nil)
     }
 
@@ -43,25 +45,21 @@ class OrderTableViewController: UITableViewController {
         cell.contentConfiguration = content
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            MenuController.shared.order.menuItems.remove(at: indexPath.row)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
